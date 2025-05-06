14175b445b6fe9db630e83195c977a4b04475a41
jackhejinyi <49220345+jackhejinyi@users.noreply.github.com>
2025-05-05 17:42:08 -0700

    Add files via upload

---
+++ white-space-print/scrpits/main.js
@@ -0,0 +1,5 @@
+const whitespace = document.getElementById("WhiteSpace").innerHTML;
+console.log(whitespace);
+
+const noWhiteSpace = document.getElementById("noWhiteSpace").innerHTML;
+console.log(noWhiteSpace);
